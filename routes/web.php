3357be6bbdcab7d9a09e67b077f70a8b3d4b30a5
abozhiinov <abozhiinov@gmail.com>
2022-12-06 18:57:25 +0200

    Added Main Navigation Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artists', function () {
+    return view('artists');
+});
+
+Route::get('/events', function () {
+    return view('events');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/artist', function () {
+    return view('single-artist');
+});
+
+Route::get('/club', function () {
+    return view('single-club');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
