99492f12271980c95a0570489722ee43e5fab945
abozhiinov <abozhiinov@gmail.com>
2022-12-19 13:27:37 +0200

    Single Views for Artist and Place

--- resources/views/single-artist.blade.php
+++ resources/views/single-artist.blade.php
@@ -1,9 +1,92 @@
+@php use App\Http\Controllers\ArtistController; @endphp
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
+	@php 
+	$artist = ArtistController::getArtistData( $username )[0];
+	$artist_genre = ArtistController::getArtistGenre( $artist->genre_id );
+	@endphp
+	@if($artist)
+		<div class="back">
+			<a href="{{ url('/artists') }}">< Назад</a>
+		</div>
+		<div class="artist-container">
+			@if(!empty($artist->cover_picture))
+			<img src="{{ url('images/' . $artist->cover_picture) }}" class="single-artist-thumbnail">
+			@else
+			<div class="single-artist-no-cover">
+			@endif
+			<div class="single-artist-info"> 
+				<div class="single-artist-name">
+					{{$artist->name}}
+					@if($artist->verified == 1)
+					<img class="single-artist-verified" src="{{url('/images/verified.svg')}}">
+					@endif
+				</div>
+				@if(!empty($artist_genre))
+				<p class="single-artist-genre">{{$artist_genre->genre}}</p>
+				@endif
+			</div>
+			<a href="{{ url($artist->facebook) }}">
+				<img class="single-artist-social facebook" src="{{url('/images/facebook.svg')}}">
+			</a>
+			<a href="{{ url($artist->instagram) }}">
+				<img class="single-artist-social instagram" src="{{url('/images/instagram.svg')}}">
+			</a>
+			<a href="{{ url($artist->youtube) }}">
+				<img class="single-artist-social youtube" src="{{url('/images/youtube.svg')}}">
+			</a>
+		</div>
+		@if(empty($artist->cover_picture))
+		</div>
+		@endif
 
+		@php 
+		$events = ArtistController::getArtistEvents( $artist->id );
+		@endphp
+		@if( ! empty( $events ) )
+		<h4 class="mx-4">Предстоящи изяви</h4>
+		<div class="upcoming-events">
+			@foreach( $events as $event )
+			<div class="event-box">
+				<img src="{{ url('images/' . $event['poster']  ) }}" class="event-thumbnail">
+				<button class="event-book button-custom">Запази</button>
+				<div class="event-info">
+					<p class="event-title">{{$event["title"]}}</p>
+					<p class="event-date">{{$event["event_date"]}}</p>
+				</div>
+			</div>
+			@endforeach
+		</div>
+		@else
+			<h5 class="no-events">{{$artist->name}} няма предстоящи изяви за момента.</h5>
+		@endif
 
-
+		@php $artists = ArtistController::otherArtists( $artist->id, $artist->genre_id ) @endphp
+		@if(count($artists) > 0)
+		<h4 class="mx-4">Други изпълнители</h4>
+		<div class="other-artists">
+			@foreach($artists as $artist)
+				<div class="artist-box">
+					<a href="/artist/{{$artist->username}}">
+						<img src="{{ url('images/' . $artist->profile_picture) }}" class="artist-thumbnail">
+						<div class="artist-box-content">
+							<p class="artist-title">
+								{{$artist->name}}
+								<span>
+								@if($artist->verified == 1)
+								<img class="artist-verified" src="{{url('/images/verified.svg')}}">
+								@endif
+								</span>
+							</p>
+						</div>
+					</a>
+				</div>
+			@endforeach
+		@endif
+		</div>
+	@endif
 </div>
 @endsection
---
+++ resources/views/single-place.blade.php
@@ -0,0 +1,87 @@
+@php use App\Http\Controllers\PlaceController; @endphp
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @php 
+	$place = PlaceController::getPlaceData( $name )[0];
+	$place_genre = PlaceController::getPlaceGenre( $place->genre_id );
+	@endphp
+	<div class="back">
+		<a href="{{ url('/places') }}">< Назад</a>
+	</div>
+	<div class="place-container">
+		@if(!empty($place->cover_picture))
+		<img src="{{ url('images/' . $place->cover_picture) }}" class="single-place-thumbnail">
+		@else
+		<div class="single-place-no-cover">
+		@endif
+		<div class="single-place-info"> 
+			<div class="single-place-title">
+				{{$place->name}}
+				@if($place->verified == 1)
+				<img class="single-place-verified" src="{{url('/images/verified.svg')}}">
+				@endif
+			</div>
+			@if(!empty($place_genre))
+			<p class="single-place-genre">{{$place_genre->genre}}</p>
+			@endif
+		</div>
+		<a href="{{ url($place->facebook) }}">
+			<img class="single-place-social facebook" src="{{url('/images/facebook.svg')}}">
+		</a>
+		<a href="{{ url($place->instagram) }}">
+			<img class="single-place-social instagram" src="{{url('/images/instagram.svg')}}">
+		</a>
+		<a href="{{ url($place->youtube) }}">
+			<img class="single-place-social youtube" src="{{url('/images/youtube.svg')}}">
+		</a>
+	</div>
+	@if(empty($place->cover_picture))
+	</div>
+	@endif
+
+	@php 
+	$events = PlaceController::getPlaceEvents( $place->id );
+	@endphp
+	@if( ! empty( $events ) )
+	<h4 class="mx-4">Предстоящи събития</h4>
+	<div class="upcoming-events">
+		@foreach( $events as $event )
+			<div class="event-box">
+				<img src="{{ url('images/' . $event['poster']  ) }}" class="event-thumbnail">
+				<button class="event-book button-custom">Запази</button>
+				<div class="event-info">
+					<p class="event-title">{{$event["title"]}}</p>
+					<p class="event-date">{{$event["event_date"]}}</p>
+				</div>
+			</div>
+			@endforeach
+	</div>
+	@else
+		<h5 class="no-events">В {{$place->name}} няма предстоящи събития за момента.</h5>
+	@endif
+
+	
+	@php $places = PlaceController::otherPlaces( $place->id, $place->genre_id ) @endphp
+	@if(count($places) > 0)
+	<h4 class="mx-4">Други заведения</h4>
+	<div class="other-artists">
+		@foreach($places as $place)
+			<div class="place-box">
+				<a href="/place/{{$place->name}}">
+					<span class="place-name">
+					{{$place->name}}
+					@if($place->verified == 1)
+					<img class="place-verified" src="{{url('/images/verified.svg')}}">
+					@endif
+					</span>
+					<img src="{{ url('images/' . $place->profile_picture) }}" class="artist-thumbnail">
+				</a>
+			</div>
+		@endforeach
+	@endif
+	</div>
+</div>
+@endsection
