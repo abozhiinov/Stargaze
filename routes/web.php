a153466b21a2ac7022c2820cbb47a53b6f11356f
abozhiinov <abozhiinov@gmail.com>
2023-01-06 10:31:54 +0200

    Ajax Routes

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,14 @@ Route::get('/register', function () {
     return view('register');
 });
 
-Route::get('/profile/{id}', function () {
+Route::get('/profile', function () {
     return view( 'my-profile' );
 });
 
 Route::get( '/artist/{username}', '\App\Http\Controllers\ArtistController@getArtistView' );
+Route::get( '/artist/{username}/invitations', '\App\Http\Controllers\ArtistController@getArtistInvitationsView' );
 
-Route::get( '/place/{name}', '\App\Http\Controllers\PlaceController@getPlaceView' );
+Route::get( '/place/{username}', '\App\Http\Controllers\PlaceController@getPlaceView' );
 
 Auth::routes();
 
@@ -56,3 +57,12 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get( '/dashboardGenreSort/{type}/{id}/{order?}/{search?}', '\App\Http\Controllers\AjaxController@dashboardGenreSort' );
 Route::get( '/dashboardOrder/{type}/{order}/{id?}/{search?}', '\App\Http\Controllers\AjaxController@dashboardOrder' );
 Route::get( '/dashboardSearch/{type}/{search}/{id?}/{order?}', '\App\Http\Controllers\AjaxController@dashboardSearch' );
+
+Route::get( '/addNewArtist/{admin_id}/{name}/{username}/{genre_id}/{profile_pic}/{cover_pic}/{facebook?}/{instagram?}/{youtube?}', '\App\Http\Controllers\AjaxController@addNewArtist' );
+Route::get( '/editArtist/{admin_id}/{name}/{username}/{genre_id}/{profile_pic}/{cover_pic}/{facebook?}/{instagram?}/{youtube?}', '\App\Http\Controllers\AjaxController@updateEditedArtist' );
+Route::get( '/addNewPlace/{admin_id}/{name}/{username}/{genre_id}/{location_id}/{profile_pic}/{cover_pic}/{facebook?}/{instagram?}/{youtube?}', '\App\Http\Controllers\AjaxController@addNewPlace' );
+
+Route::get( '/artistInvitations/{username}', '\App\Http\Controllers\AjaxController@loadArtistInvitations' );
+Route::get( '/artistEdit/{username}', '\App\Http\Controllers\AjaxController@editArtist' );
+Route::get( '/artistDelete/{username}', '\App\Http\Controllers\AjaxController@deleteArtist' );
+Route::get( '/placeDelete/{username}', '\App\Http\Controllers\AjaxController@deletePlace' );
