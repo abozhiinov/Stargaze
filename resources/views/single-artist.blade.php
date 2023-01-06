a876dca4669b4659767c348097b24629c86080b4
abozhiinov <abozhiinov@gmail.com>
2023-01-06 10:32:06 +0200

    Single View - Admin Panel

--- resources/views/single-artist.blade.php
+++ resources/views/single-artist.blade.php
@@ -1,4 +1,7 @@
-@php use App\Http\Controllers\ArtistController; @endphp
+@php 
+	use App\Http\Controllers\ArtistController; 
+	use App\Http\Controllers\PlaceController; 
+@endphp
 
 @extends('layouts.app')
 
@@ -7,13 +10,35 @@
 	@php 
 	$artist = ArtistController::getArtistData( $username )[0];
 	$artist_genre = ArtistController::getArtistGenre( $artist->genre_id );
+	$is_current_admin = 0;
 	@endphp
+	@auth
+		@php $is_current_admin = ( $artist->admin_id === auth()->user()->id ); @endphp
+	@endauth
 	@if($artist)
 		<div class="back">
 			<a href="{{ url('/artists') }}">< Назад</a>
 		</div>
+
+		@auth
+		@if( $is_current_admin ) 
+		<div class="admin-panel d-flex justify-content-around">
+			<button class="artist-see-invitations" data-username={{$username}}>{{_('Виж покани')}}</button>
+			<button class="artist-edit" data-username={{$username}}>{{_('Редактирай изпълнител')}}</button>
+			<button class="artist-delete">{{_('Изтрий изпълнител')}}</button>
+		</div>
+		@else
+			@php $has_places = PlaceController::getAdminPlaces( auth()->user()->id ); @endphp
+			@if ( count($has_places) )
+			<div class="admin-panel d-flex justify-content-around">
+				<button class="artist-invite">{{_('Покани изпълнител')}}</button>
+			</div>
+			@endif
+		@endif
+		@endauth
+
 		<div class="artist-container">
-			@if(!empty($artist->cover_picture))
+			@if( $artist->cover_picture )
 			<img src="{{ url('images/' . $artist->cover_picture) }}" class="single-artist-thumbnail">
 			@else
 			<div class="single-artist-no-cover">
@@ -21,38 +46,74 @@
 			<div class="single-artist-info"> 
 				<div class="single-artist-name">
 					{{$artist->name}}
-					@if($artist->verified == 1)
+					@if( $artist->verified )
 					<img class="single-artist-verified" src="{{url('/images/verified.svg')}}">
 					@endif
 				</div>
-				@if(!empty($artist_genre))
+				@if( $artist_genre )
 				<p class="single-artist-genre">{{$artist_genre->name}}</p>
 				@endif
 			</div>
+			<div class="artist-box-likes">
+				<img class="artist-likes" src="{{url('/images/likes.svg')}}">
+				<p class="artist-likes-count"> {{$artist->likes}}</p>
+			</div>
+			@if ( $artist->facebook )
 			<a href="{{ url($artist->facebook) }}">
 				<img class="single-artist-social facebook" src="{{url('/images/facebook.svg')}}">
 			</a>
+			@endif
+			@if ( $artist->instagram )
 			<a href="{{ url($artist->instagram) }}">
 				<img class="single-artist-social instagram" src="{{url('/images/instagram.svg')}}">
 			</a>
+			@endif
+			@if ( $artist->youtube )
 			<a href="{{ url($artist->youtube) }}">
 				<img class="single-artist-social youtube" src="{{url('/images/youtube.svg')}}">
 			</a>
+			@endif
 		</div>
 		@if(empty($artist->cover_picture))
 		</div>
 		@endif
 
-		@php 
-		$events = ArtistController::getArtistEvents( $artist->id );
-		@endphp
-		@if( ! empty( $events ) )
-		<h4 class="mx-4">Предстоящи изяви</h4>
-		<div class="upcoming-events">
-			@foreach( $events as $event )
-			<div class="event-box">
-				<a>
-				<div class="event-box-content">
+		{{-- https://stackoverflow.com/questions/60880366/laravel-bootstrap-delete-confirmation-using-modal --}}
+		<!-- Delete Warning Modal -->
+		<div class="modal modal-danger fade" id="deleteArtistModal" tabindex="-1" role="dialog" aria-labelledby="Delete" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<div class=""></div>
+						<h5 class="modal-title" id="deleteArtistModalLabel">Изтриване на изпълнител</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form id="form-delete-artist" data-username={{$username}} onsubmit="return false">
+							@csrf
+							@method('DELETE')
+							<h5 id="delete-artist-text" class="text-center text-white ">Сигурни ли сте че искате да изтриете този изпълнител?</h5>
+							<input id="delete-artist-username" type="hidden">
+							<button id="delete-artist-yes" type="submit" class="btn btn-sm btn-danger">Да</button>
+							<button id="delete-artist-no" type="button" class="btn btn-secondary" data-dismiss="modal">Не</button>
+							<button id="delete-artist-ok" class="button-custom" hidden>ОК!</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="artist-content">
+			@php 
+			$events = ArtistController::getArtistEvents( $artist->id );
+			@endphp
+			@if( ! empty( $events ) )
+			<h4 class="mx-4">Предстоящи изяви</h4>
+			<div class="upcoming-events">
+				@foreach( $events as $event )
+				<div class="event-box">
 					<img src="{{ url('images/' . $event['poster']  ) }}" class="event-thumbnail">
 					<button class="event-book button-custom">Запази</button>
 					<div class="event-info">
@@ -60,40 +121,41 @@
 						<p class="event-date">{{$event["event_date"]}}</p>
 					</div>
 				</div>
-				</a>
+				@endforeach
 			</div>
-			@endforeach
-		</div>
-		@else
-			<h5 class="no-events">{{$artist->name}} няма предстоящи изяви за момента.</h5>
-		@endif
+			@else
+				<h4 class="no-events">{{$artist->name}} няма предстоящи изяви за момента.</h4>
+			@endif
 
-		@php $artists = ArtistController::otherArtists( $artist->id, $artist->genre_id ) @endphp
-		@if(count($artists) > 0)
-		<h4 class="mx-4">Други изпълнители</h4>
-		<div class="other-artists">
-			@foreach($artists as $artist)
-				<div class="artist-box">
-					<a href="/artist/{{$artist->username}}">
-						<img src="{{ url('images/' . $artist->profile_picture) }}" class="artist-thumbnail">
-						<div class="artist-box-content">
-							<div class="artist-box-likes">
-								<img class="artist-likes" src="{{url('/images/likes.svg')}}">
-								<p class="artist-likes-count"> {{$artist->likes}}</p>
-							</div>
-							<p class="artist-title">
-								{{$artist->name}}
-								<span>
-								@if($artist->verified == 1)
-								<img class="artist-verified" src="{{url('/images/verified.svg')}}">
-								@endif
-								</span>
-							</p>
+			@if( ! $is_current_admin ) 
+				@php $artists = ArtistController::otherArtists( $artist->id, $artist->genre_id ) @endphp
+				@if(count($artists) > 0)
+					<h4 class="mx-4">Други изпълнители</h4>
+					<div class="other-artists">
+						@foreach($artists as $artist)
+						<div class="artist-box">
+							<a href="/artist/{{$artist->username}}">
+								<img src="{{ url('images/' . $artist->profile_picture) }}" class="artist-thumbnail">
+								<div class="artist-box-likes">
+									<img class="artist-likes" src="{{url('/images/likes.svg')}}">
+									<p class="artist-likes-count"> {{$artist->likes}}</p>
+								</div>
+								<div class="artist-box-content">
+									<p class="artist-title">
+										{{$artist->name}}
+										<span>
+										@if($artist->verified == 1)
+										<img class="artist-verified" src="{{url('/images/verified.svg')}}">
+										@endif
+										</span>
+									</p>
+								</div>
+							</a>
 						</div>
-					</a>
-				</div>
-			@endforeach
-		@endif
+						@endforeach
+				@endif
+					</div>
+			@endif
 		</div>
 	@endif
 </div>
--- resources/views/single-place.blade.php
+++ resources/views/single-place.blade.php
@@ -5,12 +5,53 @@
 @section('content')
 <div class="container">
     @php 
-	$place = PlaceController::getPlaceData( $name )[0];
+	$place = PlaceController::getPlaceData( $username )[0];
 	$place_genre = PlaceController::getPlaceGenre( $place->genre_id );
+	$is_current_admin = 0;
 	@endphp
+	@auth
+		@php $is_current_admin = ( $place->admin_id === auth()->user()->id ); @endphp
+	@endauth
 	<div class="back">
 		<a href="{{ url('/places') }}">< Назад</a>
 	</div>
+
+	@auth
+	@if( $is_current_admin ) 
+	<div class="admin-panel d-flex justify-content-around">
+		<button class="place-see-invitations" data-username={{$username}}>{{_('Виж изпратени покани')}}</button>
+		<button class="place-edit">{{_('Редактирай заведение')}}</button>
+		<button class="place-delete">{{_('Изтрий заведение')}}</button>
+	</div>
+	@endif
+	@endauth
+
+	{{-- https://stackoverflow.com/questions/60880366/laravel-bootstrap-delete-confirmation-using-modal --}}
+		<!-- Delete Warning Modal -->
+		<div class="modal modal-danger fade" id="deletePlaceModal" tabindex="-1" role="dialog" aria-labelledby="Delete" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<div class=""></div>
+						<h5 class="modal-title" id="deletePlaceModalLabel">Изтриване на заведение</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form id="form-delete-place" data-username={{$username}} onsubmit="return false">
+							@csrf
+							@method('DELETE')
+							<h5 id="delete-place-text" class="text-center text-white ">Сигурни ли сте че искате да изтриете това заведение?</h5>
+							<button id="delete-place-yes" type="submit" class="btn btn-sm btn-danger">Да</button>
+							<button id="delete-place-no" type="button" class="btn btn-secondary" data-dismiss="modal">Не</button>
+							<button id="delete-place-ok" class="button-custom" hidden>ОК!</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	<div class="place-container">
 		@if(!empty($place->cover_picture))
 		<img src="{{ url('images/' . $place->cover_picture) }}" class="single-place-thumbnail">
@@ -25,8 +66,12 @@
 				@endif
 			</div>
 			@if(!empty($place_genre))
-			<p class="single-place-genre">{{$place_genre->genre}}</p>
+			<p class="single-place-genre">{{$place_genre->name}}</p>
 			@endif
+		</div>
+		<div class="place-box-likes">
+			<img class="place-likes" src="{{url('/images/likes.svg')}}">
+			<p class="place-likes-count"> {{$place->likes}}</p>
 		</div>
 		<a href="{{ url($place->facebook) }}">
 			<img class="single-place-social facebook" src="{{url('/images/facebook.svg')}}">
@@ -70,7 +115,7 @@
 	<div class="other-artists">
 		@foreach($places as $place)
 			<div class="place-box">
-				<a href="/place/{{$place->name}}">
+				<a href="/place/{{$place->username}}">
 					<img src="{{ url('images/' . $place->profile_picture) }}" class="place-thumbnail">
 					<div class="place-box-content">
 						<p class="place-title">
