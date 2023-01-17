9049a068a63c8b89a430215fdd4a52a78eabd7e1
abozhiinov <abozhiinov@gmail.com>
2023-01-17 22:15:53 +0200

    events dashboard markup fix & filters

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,22 +1,49 @@
-@php use App\Http\Controllers\EventController; @endphp
+@php 
+use App\Http\Controllers\EventController; 
+use App\Http\Controllers\PlaceController;
+use App\Http\Controllers\ArtistController;
+@endphp
 
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
 	<h2 class="text-center dashboard-title">Събития</h2>
+	@php 
+		$locations = PlaceController::getAllLocations(); 
+		$genres = ArtistController::getAllGenres();
+	@endphp
 	<form>
-		<div class="form-container d-flex justify-content-around p-4 mt-5">
-			<input type="text" class="search-input-field" placeholder="Търси събитие...">
-			<select class="search-input-field">
-				<option value="" disabled selected>Филтри</option>
-			</select>
-			<select class="search-input-field">
-				<option disabled selected>Сортирай</option>
-				<option>Подреди по азбучен ред</option>
-				<option>Подреди по популярност</option>
-			</select>
-		</div>
+		<form onsubmit="return false">
+			<input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+			<div class="form-container justify-content-around row p-4 mt-5">
+				<div class="row justify-content-around">
+					<input type="text" class="search-input-field col-sm-5 search-dashboard events" id="search-dashboard" placeholder="Търси по изпълнител...">
+					<input type="text" class="search-input-field col-sm-5 search-dashboard events" id="search-dashboard" placeholder="Търси по заведение...">
+				</div>
+				<div class="row justify-content-around">
+					<select class="search-input-field col-sm-3 sort-genres-dashboard events" id="sort-genres-dashboard">
+						<option value="all-places" selected>Всички локации</option>
+						@foreach ( $locations as $location ) 
+						<option value="{{$location->id}}">{{$location->name}}</option>
+						@endforeach
+					</select>
+					<select class="search-input-field col-sm-3 sort-genres-dashboard events" id="sort-genres-dashboard">
+						<option value="all-places" selected>Всички жанрове</option>
+						@foreach ( $genres as $genre ) 
+						<option value="{{$genre->id}}">{{$genre->name}}</option>
+						@endforeach
+					</select>
+					<select class="search-input-field col-sm-3 order-dashboard events" id="order-dashboard">
+						<option value="no-sort" selected>По подразбиране</option>
+						<option value="alphabet-start">{{_('По дата')}}</option>
+						<option value="alphabet-end">{{_('По азбучен ред - низходящо')}}</option>
+						<option value="popular">{{_('По популярност - низходящо')}}</option>
+						<option value="unpopular">{{_('По популярност - възходящо')}}</option>
+					</select>
+				</div>
+			</div>
+		</form>
 	</form>
 
 	@php $events = EventController::allEvents(); @endphp
@@ -24,16 +51,11 @@
 	@if(count($events) > 0)
 		@foreach($events as $event)
 			<div class="event-box">
-				<a>
+				<img src="{{ url('images/' . $event['poster']  ) }}" class="event-thumbnail">
 				<div class="event-box-content">
-					<img src="{{ url('images/' . $event['poster']  ) }}" class="event-thumbnail">
-					{{-- <div class="event-info"> --}}
-						<p class="event-title">{{$event["title"]}}</p>
-						<p class="event-date">{{$event["event_date"]}}</p>
-					{{-- </div> --}}
-					<button class="event-book button-custom">Запази</button>
+					<p class="event-title">{{$event["title"]}}</p>
+					<p class="event-date">{{$event["event_date"]}}</p>
 				</div>
-				</a>
 			</div>
 		@endforeach
 	@endif
